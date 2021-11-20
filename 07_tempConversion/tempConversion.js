35aed09091b9632e3dc0f6edf99e790f96b3783a
apoorwaG <apoogiri@gmail.com>
2021-11-19 18:34:21 -0800

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degF) {
+  //  [°C] = ([°F] − 32) × 5⁄9
+  let degC = Math.round((degF - 32) * (5/9) * 10) / 10;
+  return degC;
 };
 
-const ctof = function() {
-
+const ctof = function(degC) {
+  //  [°F] = [°C] × 9/5 + 32
+  let degF = Math.round((degC* 9/5 + 32)* 10) / 10;
+  return degF;
 };
 
 // Do not edit below this line
