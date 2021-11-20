59547b5fd880907a8f53b9e7b90010368fc5d5f2
apoorwaG <apoogiri@gmail.com>
2021-11-19 17:21:01 -0800

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    if(typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+
+    let temp;
+    if(num1 > num2){
+        temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    
+    if(num1 === num2) {
+        return num1;
+    }
+
+    for(let i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
