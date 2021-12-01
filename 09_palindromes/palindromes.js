d787eb6f17258a1f486d81bbc55d2a52b60f95e5
apoorwaG <apoogiri@gmail.com>
2021-12-01 14:30:31 -0800

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (given_string) {
+    const punctuations = [',', '.', '!', '?', ';', '\'', '"', " "];
+    let punctuationSet = new Set(punctuations);
 
+    // convert the string to an array of characters
+    const str_array = given_string.toLowerCase().split("");
+    // filter out the punctuations and spaces
+    const filtered_array = str_array.filter((character) => {
+        return !(punctuationSet.has(character));
+    });
+
+    // join the string array
+    const array_length = filtered_array.length;
+    for(let i = 0; i < array_length/2; i++){
+        if(filtered_array[i] !== filtered_array[array_length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+    
 };
 
 // Do not edit below this line
